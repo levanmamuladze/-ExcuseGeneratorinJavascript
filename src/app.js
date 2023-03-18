9513753bed99ca42e64f1c0c5dc201c5e89b23cb
levanmamuladze <levanmamoulatze@gmail.com>
2023-03-18 16:01:49 +0000

    ExcuseGenerator Done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let whoIndx = Math.floor(Math.random() * who.length);
+    let actionIndx = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoIndx] +
+      " " +
+      action[actionIndx] +
+      " " +
+      what[whatIndex] +
+      " " +
+      when[whenIndex]
+    );
+  };
+
+  let excuse = document.getElementById("excuse");
+  let generateBtn = document.getElementById("generate-btn");
+  generateBtn.addEventListener("click", function() {
+    excuse.textContent = generateExcuse();
+  });
 };
